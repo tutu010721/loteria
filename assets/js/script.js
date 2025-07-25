2cdce89e4afcdf2748565fbbf0adebd4fc259802
tutu010721 <sac.auramoveiseutilidades@gmail.com>
2025-07-25 18:10:50 -0300

    Create script.js

---
+++ assets/js/script.js
@@ -0,0 +1,5 @@
+// script.js
+// Por enquanto, não há necessidade de JavaScript customizado.
+// As funcionalidades são baseadas em HTML e CSS.
+// No futuro, podemos adicionar interações aqui.
+console.log("Template base carregado.");
